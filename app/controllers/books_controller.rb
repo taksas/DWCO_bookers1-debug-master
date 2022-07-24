a1f377171b3b1d8f83e894b72a10976cc528b55e
taksas <takuminakayama4869@gmail.com>
2022-07-24 07:16:40 +0000

    ~まさかのFormタグ2重~

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
@@ -26,10 +27,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update()
+    book = Book.find(params[:id])
+    if book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(book.id)
     else
       render :edit
     end
