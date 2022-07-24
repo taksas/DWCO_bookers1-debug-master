ae297d8d796cedb800d5a901899c710d24b361b4
taksas <takuminakayama4869@gmail.com>
2022-07-24 07:19:38 +0000

    後片付け

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,10 +27,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
