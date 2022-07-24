1554687be3fef245a12bf433038cb55d64d7a5cd
taksas <takuminakayama4869@gmail.com>
2022-07-24 06:12:09 +0000

    homes_controllerをController < ApplicationControllerからHomesController < ApplicationControllerhe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  get 'books'  => 'books#top', as: "start"
   resources :books
+
 end
